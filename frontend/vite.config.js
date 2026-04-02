dfc316de09f53a1b4a0e201e18e41e6afa5b94ae
Rama Raju <drramarajudentalclinic@gmail.com>
2026-04-02 20:27:49 +0530

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default defineConfig({
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
           router: ['react-router-dom'],
-          // add other big libs here (e.g. 'axios', 'chart.js')
         }
       }
     }
